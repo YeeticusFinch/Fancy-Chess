c0fa21cbce6147716d43f76bef6e142e6764c802
Carl Lerdorf <50182007+YeeticusFinch@users.noreply.github.com>
2022-07-21 12:21:44 +0200

    Update Board.cs

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    public Vector3 size = new Vector3(8, 1, 8);
+    public List<int> size = new List<int>();
+    public int dimensions = 2;
+    //public Vector3 size = new Vector3(8, 1, 8); // Depricated
     public GameObject boardSquare;
     public Color colorA;
     public Color colorB;
@@ -15,6 +17,37 @@ public class Board : MonoBehaviour
         InitBoard();
     }
 
+    void InitBoard()
+    {
+        List<int> startPos = new List<int>();
+        for (int i = 0; i < dimensions; i++)
+            startPos[i] = -size[i]/2;         // Always keep the center on 0,0   or 0,0,0      or 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        
+    }
+
+    void SpawnSquares(int index, List<int> iter)
+    {
+        if (index >= dimensions) 
+        {
+            GameObject newTile = Instantiate(boardSquare, new Vector3(iter[0]-size.x/2, iter[1]*2-size.y, iter[2]-size.z/2), Quaternion.identity);
+            int evens = 0;
+            for (int i = 0; i < iter.size(); i++)
+                evens += iter[i]%2 == 0 ? 1 : 0;
+            if (evens <= 1)
+                newTile.GetComponent<Renderer>().material.SetColor("_Color", colorA);
+            else
+                newTile.GetComponent<Renderer>().material.SetColor("_Color", colorB);
+            newTile.GetComponent<Tile>.position = new List<int>(iter);
+            return;
+        }
+        for (int i = 0; i < size[index]; i++)
+        {
+            iter[index] = i;
+            SpawnSquares(index + 1, new List<int>(iter))
+        }
+    }
+
+    /*
     void InitBoard()
     {
         Vector3 startPos = new Vector3(-size.x/2, -size.y, -size.z/2);
@@ -32,7 +65,7 @@ public class Board : MonoBehaviour
                 }
             }
         }
-    }
+    }*/
 
     // Update is called once per frame
     void Update()
