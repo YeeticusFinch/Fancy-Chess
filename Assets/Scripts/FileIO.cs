a96f06110aedcad0c7a176b0cabc2be4fd304619
YeeticusFinch <50182007+YeeticusFinch@users.noreply.github.com>
2023-04-15 16:50:23 -0400

    added the scripts

---
+++ Assets/Scripts/FileIO.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileIO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void WriteString(string text, string path)
+    {
+        //string path = "Assets/Resources/test.txt";
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(text);
+        writer.Close();
+        //Re-import the file to update the reference in the editor
+        //AssetDatabase.ImportAsset(path);
+        //TextAsset asset = Resources.Load("test");
+        //Print the text from the file
+        //Debug.Log(asset.text);
+    }
+
+    public static string ReadString(string path)
+    {
+        //string path = "Assets/Resources/test.txt";
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        string result = reader.ReadToEnd();
+        //Debug.Log(reader.ReadToEnd());
+        reader.Close();
+        return result;
+    }
+}
---
+++ Assets/Scripts/PieceLibrary.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceLibrary : MonoBehaviour
+{
+    public ChessPiece[] pieces;
+    public int index = 0;
+    public Tile square;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (ChessPiece p in pieces)
+        {
+            p.transform.localPosition = Vector3.zero;
+            p.gameObject.GetComponent<Rigidbody>().useGravity = false;
+            p.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        }
+        pieces[0].gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    public void next(int add)
+    {
+        pieces[index].GetComponent<MeshRenderer>().enabled = false;
+        index += add;
+        index %= pieces.Length;
+        if (index < 0) index = pieces.Length - index;
+        pieces[index].GetComponent<MeshRenderer>().enabled = true;
+    }
+
+    public ChessPiece getPiece()
+    {
+        return pieces[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
